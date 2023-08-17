6266fc00664dfe1e65c683bdfc74edbad6b6ec1f
Ernestek <iernestek@gmail.com>
2023-08-17 15:52:33 +0300

    builtwith seo

---
+++ seo_test/adstxt_validation_test.py
@@ -0,0 +1,28 @@
+import requests
+
+def check_ads_txt_validity(url):
+    try:
+        ads_txt_url = f"{url}/ads.txt"
+        response = requests.get(ads_txt_url)
+
+        if response.status_code == 200:
+            ads_txt_content = response.text
+            lines = ads_txt_content.split('\n')
+            print(ads_txt_content)
+            for line in lines:
+                parts = line.split(',')
+                if len(parts) != 3:
+                    return line
+                if parts[1].strip() != "DIRECT" and parts[1].strip() != "RESELLER":
+                    return line
+
+                # Additional checks could be performed here if needed
+            print("ads.txt is in a valid format.")
+        else:
+            print(f"ads.txt not found or error: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+# Replace "https://example.com" with the actual website's URL
+print(check_ads_txt_validity("http://baskino.me/"))
--- seo_test/nested_tables_test.py
+++ seo_test/nested_tables_test.py
@@ -15,6 +15,23 @@ def has_nested_tables(url):
             parent_tables = table.find_parents("table")
             if parent_tables:
                 nested_tables += 1
+                selector_parts = []
+                current_element = table
+                while current_element:
+                    if current_element.name == 'body':
+                        break
+                    if current_element.name:
+                        element_selector = current_element.name
+                        print(element_selector)
+                        if current_element.get("id"):
+                            element_selector += f'#{current_element.get("id")}'
+                        if current_element.get("class"):
+                            element_selector += f'.{".".join(current_element.get("class"))}'
+                        selector_parts.insert(0, element_selector)
+                    current_element = current_element.parent
+                selector = " ".join(selector_parts)
+                # nested_table_selectors.append(selector)
+                print(selector)
         if nested_tables > 0:
             print(f"The page has {nested_tables} nested table(s).")
         else:
